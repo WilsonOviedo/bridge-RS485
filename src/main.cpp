c66129c9e185610004302954ce4a496e91503d0c
Wilson Oviedo Hachen <wilsonoh@outlook.es>
2022-02-10 21:22:36 -0300

    funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,46 @@
 
 //#include <SoftwareSerial.h>
 
+// SoftwareSerial Serial2(7, 8); // RX, TX
 
-//SoftwareSerial Serial2(7, 8); // RX, TX
+#define DE_RE 33  // Pin de control DE/DE
+#define BAUD 300 // Velocidad de RS485
 
-#define DE_RE 33 //Pin de control DE/DE
-#define BAUD 300 //Velocidad de RS485
+#define ECHO false
 
-void preTrans(){
-  digitalWrite(DE_RE,HIGH);
+void preTrans()
+{
+  digitalWrite(DE_RE, HIGH);
 }
-void posTrans(){
-  digitalWrite(DE_RE,LOW);
-}
-void setup() {
-  pinMode(DE_RE,OUTPUT);
-  Serial.begin(9600,SERIAL_7E1);
-  while (!Serial) {
-    ; 
-  }
-  Serial2.begin(BAUD,SERIAL_7E1);
+void posTrans()
+{
+  digitalWrite(DE_RE, LOW);
 }
 
-void loop() { 
-    
-  while (Serial2.available()) {
-    Serial.write(Serial2.read());
+
+void setup()
+{
+  pinMode(DE_RE, OUTPUT);
+  posTrans();
+  Serial.begin(9600, SERIAL_7E1);
+  // Serial.begin(BAUD);
+  while (!Serial)
+  {
+    ;
   }
-  while(Serial.available()) {
-    preTrans();
-    Serial2.write(Serial.read());    
-  }
+  Serial2.begin(BAUD, SERIAL_7E1);
+  Serial.write("Start!...");
+  preTrans();
+  Serial2.write("/?02745281!\r\n");
   Serial2.flush();
   posTrans();
+}
+
+void loop()
+{
   
+while (Serial2.available())
+  {
+    Serial.write(Serial2.read());
+  }
 }
